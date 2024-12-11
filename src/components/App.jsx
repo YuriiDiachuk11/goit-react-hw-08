d297994612d1a929214e2449934c81ce8ba8c392
YD <yuriidiachuk@icloud.com>
2024-12-11 21:28:38 +0200

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,58 @@
 import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Layout/Layout";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsRefreshing } from "../redux/auth/selectors";
 import { refreshUser } from "../redux/auth/operations";
+import { selectIsRefreshing } from "../redux/auth/selectors";
+import RestrictedRoute from "./RestrictedRoute/RestrictedRoute";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import Layout from "./Layout/Layout";
 
-const Home = lazy(() => import("../pages/HomePage"));
+const HomePage = lazy(() => import("../pages/HomePage"));
 const ContactsPage = lazy(() => import("../pages/ContactsPage"));
-const Login = lazy(() => import("../pages/LoginPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
 const RegistrationPage = lazy(() => import("../pages/RegistrationPage"));
 
 function App() {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
+
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
-  return isRefreshing ? null : (
+
+  return isRefreshing ? (
+    <p>Refreshing user...</p>
+  ) : (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route index element={<HomePage />} />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login">
+                <ContactsPage />
+              </PrivateRoute>
+            }
+          />
         </Route>
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<div>404: Page Not Found</div>} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/contacts">
+              <RegistrationPage />
+            </RestrictedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts">
+              <LoginPage />
+            </RestrictedRoute>
+          }
+        />
+        <Route path="*" element={<p>404: Page Not Found</p>} />
       </Routes>
     </Suspense>
   );
