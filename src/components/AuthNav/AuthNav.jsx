be017369d3f736f409c3d08290d99974c69ac28e
YD <yuriidiachuk@icloud.com>
2024-12-14 00:07:00 +0200

    update AuthNav

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,6 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const AuthNav = () => {
+  const location = useLocation();
+  if (location.pathname === "/") {
+    return null;
+  }
   return (
     <div>
       <NavLink to="/register">Register</NavLink>
