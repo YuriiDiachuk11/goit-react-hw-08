c6da528bfe8fff7c62f72ad1d6f7d67c149079bf
YD <yuriidiachuk@icloud.com>
2024-12-14 01:05:00 +0200

    add validation to RegistrationForm

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -4,10 +4,23 @@ import { useDispatch } from "react-redux";
 import { register } from "../../redux/auth/operations";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Name is empty"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6)
+    .matches(/[0-9]/, "Password must include a number")
+    .required("Password is required"),
+});
 
 const RegistrationForm = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleSubmit = (values, options) => {
     dispatch(register(values))
       .unwrap()
@@ -20,50 +33,71 @@ const RegistrationForm = () => {
       });
     options.resetForm();
   };
+
   const initialValues = {
     name: "",
     email: "",
     password: "",
   };
+
   return (
     <div className={s.wrapper}>
       <h2 className={s.title}>Create an Account</h2>
-      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-        <Form className={s.form}>
-          <label htmlFor="name" className={s.label}>
-            Name
-          </label>
-          <Field
-            className={s.input}
-            id="name"
-            name="name"
-            placeholder="Enter name"
-          />
-          <label htmlFor="email" className={s.label}>
-            Email
-          </label>
-          <Field
-            className={s.input}
-            id="email"
-            name="email"
-            placeholder="Enter email"
-          />
-          <label htmlFor="password" className={s.label}>
-            Password
-          </label>
-          <Field
-            id="password"
-            className={s.input}
-            name="password"
-            type="password"
-            placeholder="Enter pass"
-          />
-          <button className={s.button} type="submit">
-            Submit
-          </button>
-        </Form>
+      <Formik
+        onSubmit={handleSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        {({ errors, touched }) => (
+          <Form className={s.form}>
+            <label htmlFor="name" className={s.label}>
+              Name
+            </label>
+            <Field
+              className={s.input}
+              id="name"
+              name="name"
+              placeholder="Enter name"
+            />
+            {errors.name && touched.name && (
+              <div className={s.error}>{errors.name}</div>
+            )}
+
+            <label htmlFor="email" className={s.label}>
+              Email
+            </label>
+            <Field
+              className={s.input}
+              id="email"
+              name="email"
+              placeholder="Enter email"
+            />
+            {errors.email && touched.email && (
+              <div className={s.error}>{errors.email}</div>
+            )}
+
+            <label htmlFor="password" className={s.label}>
+              Password
+            </label>
+            <Field
+              id="password"
+              className={s.input}
+              name="password"
+              type="password"
+              placeholder="Enter password"
+            />
+            {errors.password && touched.password && (
+              <div className={s.error}>{errors.password}</div>
+            )}
+
+            <button className={s.button} type="submit">
+              Submit
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
 };
+
 export default RegistrationForm;
